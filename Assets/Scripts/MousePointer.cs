e9c25809425d0de56b59fdeb9ae2bb37e845d3a3
keonen <keijo.olavi.kangas@gmail.com>
2015-08-04 12:59:12 +0300

    2015-08-04 13:00

---
+++ Assets/Scripts/MousePointer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MousePointer : MonoBehaviour 
+{
+	public Texture2D cursorImage;
+	
+	private int cursorWidth = 32;
+	private int cursorHeight = 32;
+	
+	void Start()
+	{
+		Cursor.visible = false;
+		//Screen.showCursor = false;
+	}
+	
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.R))
+		{			
+			Application.LoadLevel(Application.loadedLevel);
+		}
+		
+		if (Input.GetKeyDown(KeyCode.L))
+		{			
+			Cursor.lockState = CursorLockMode.Locked;
+		}
+		
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{			
+			Cursor.lockState = CursorLockMode.None;
+		}
+
+		if (Input.GetKeyDown(KeyCode.O))
+		{			
+			Cursor.lockState = CursorLockMode.None;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Q))
+		{			
+			Application.Quit();
+		}
+	}
+
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect(Input.mousePosition.x - 16, Screen.height - Input.mousePosition.y - 16, cursorWidth, cursorHeight), cursorImage);
+	}
+}
---
+++ Assets/Scripts/SpawnLegoCubes.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnLegoCubes : MonoBehaviour {
+
+	public GameObject peliobjekti_1;
+	public GameObject peliobjekti_2;
+	public GameObject peliobjekti_3;
+
+	// Use this for initialization
+	void Start () 
+	{
+		for (int y = 0; y < 9; y++) {
+			for (int x = 0; x < 7; x++) {
+				float spawnHeight = 0.51f;
+				float spawnWidth = 0.64f;
+				if (y % 6 == 0)
+				{
+					Instantiate(peliobjekti_1, new Vector3(spawnWidth * x + 250f, spawnHeight * y + 0.5f, 250f), Quaternion.identity);
+					Instantiate(peliobjekti_1, new Vector3(250 - 0.16f, spawnHeight * y + 0.5f, spawnWidth * x + 250.50f), Quaternion.Euler(0, 90, 0));
+				}
+				else if (y % 6 == 1)
+				{
+					Instantiate(peliobjekti_2, new Vector3(spawnWidth * x + 250.32f, spawnHeight * y + 0.5f, 250f), Quaternion.identity);
+					Instantiate(peliobjekti_2, new Vector3(250 - 0.16f, spawnHeight * y + 0.5f, spawnWidth * x + 250.16f), Quaternion.Euler(0, 90, 0));
+				}
+				else if (y % 6 == 2)
+				{
+					Instantiate(peliobjekti_3, new Vector3(spawnWidth * x + 250, spawnHeight * y + 0.5f, 250), Quaternion.identity);
+					Instantiate(peliobjekti_3, new Vector3(250 - 0.16f, spawnHeight * y + 0.5f, spawnWidth * x + 250.50f), Quaternion.Euler(0, 90, 0));
+				}
+				else if (y % 6 == 3)
+				{
+					Instantiate(peliobjekti_1, new Vector3(spawnWidth * x + 250.32f, spawnHeight * y + 0.5f, 250), Quaternion.identity);
+					Instantiate(peliobjekti_1, new Vector3(250 - 0.16f, spawnHeight * y + 0.5f, spawnWidth * x + 250.16f), Quaternion.Euler(0, 90, 0));
+				}
+				else if (y % 6 == 4)
+				{
+					Instantiate(peliobjekti_2, new Vector3(spawnWidth * x + 250, spawnHeight * y + 0.5f, 250), Quaternion.identity);
+					Instantiate(peliobjekti_2, new Vector3(250 - 0.16f, spawnHeight * y + 0.5f, spawnWidth * x + 250.50f), Quaternion.Euler(0, 90, 0));
+				}
+				else if (y % 6 == 5)
+				{
+					Instantiate(peliobjekti_3, new Vector3(spawnWidth * x + 250.32f, spawnHeight * y + 0.5f, 250), Quaternion.identity);
+					Instantiate(peliobjekti_3, new Vector3(250 - 0.16f, spawnHeight * y + 0.5f, spawnWidth * x + 250.16f), Quaternion.Euler(0, 90, 0));
+				}
+			
+			}
+		}
+	
+	}
+		
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+}
